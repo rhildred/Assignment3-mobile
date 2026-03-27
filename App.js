14ad2f0fc823e2fe8f9e13a73a5e21c0841490f7
Richard Hildred <rhildred@gmail.com>
2026-03-27 00:14:39 +0000

    sort of working

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Rich was here</Text>
       <Text>So there...</Text>
       <StatusBar style="auto" />
     </View>
